e3dea5e9dc3d8f6bfa4add4d4e9a394bb0833549
ozdemirmzn <ozdemirmzn@gmail.com>
2020-10-01 20:33:52 -0500

    day 2

---
+++ Day-2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Day_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Clear();
+            string str = User_Input.getUserInput();
+
+            //string str = string.Empty;
+            //str = "website";
+
+            while (str.Length > 0)
+            {
+                Console.Write(str[0] + ": ");
+                int count = 0;
+
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (str[0] == str[i])
+                    {
+                        count++;
+                    }
+                }
+
+                Console.WriteLine(count);
+
+                //returns a new string without counted chars
+                str = str.Replace(str[0].ToString(), string.Empty);
+                 // Console.WriteLine(str);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ Day-2/User-Input.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Day_2
+{
+    public class User_Input
+    {
+        public static string getUserInput()
+        {
+            string input = string.Empty;
+            input = Console.ReadLine();
+            return input;
+        }
+    }
+}
